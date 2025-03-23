47eee2c9ed96d357486843c2db17db657ae968b7
RuMe6 <rustam.meshkin@gmail.com>
2025-03-24 00:59:54 +0500

    Homework of Classes

---
+++ Otus_Classes-Stack/Otus_Classes/Program.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Otus_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StackClass sc = new StackClass("a", "b", "c");
+            
+            bool start = true;
+
+            while(start)
+            {
+                Console.WriteLine("For the adding items to Stack, Enter '1'.\n" +
+                "For the deleting the items from Stack, Enter '2'.\n" +
+                "For exit, Enter '9'.\n");
+
+                byte temp = 0;
+                string? num = Console.ReadLine();
+                bool boolNum = byte.TryParse(num, out byte result);
+                if (boolNum == true)
+                {
+                    if ((result > 0) && (result < 3)) temp = result;
+                    if (result == 9)
+                    {
+                        start = false;
+                        break;
+                    }
+                }
+
+                switch (temp)
+                {
+                    case 1:
+                        Console.WriteLine("\nPlease enter the item to adding....\n");
+                        string? addData = Console.ReadLine();
+                        sc.AddData(addData);
+                        break;
+                    case 2:
+                        sc.PopData();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Otus_Classes-Stack/Otus_Classes/StackClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Otus_Classes
+{
+    public class StackClass
+    {
+        List<string> data = new List<string>();
+
+        public int Size 
+        { 
+            get => data.Count; 
+        }
+        public string? Top
+        {
+            get
+            {
+                return data[Size - 1];
+            }
+        }
+
+        public StackClass(params string[] temp)
+        {
+            for (int i = 0; i < temp.Length; i++ )
+            {
+                data.Add(temp[i]);
+                Console.WriteLine($"Word '{temp[i]}' was added to List.\n" +
+                    $"List have {i + 1} element");
+            }
+        }
+
+        public void AddData(string element)
+        {
+            data.Add(element);
+            Console.WriteLine($"\n\tThe '{element}' was added to Stack. STACK size = {Size}\n");
+        }
+
+        public void PopData()
+        {
+            try
+            {
+                var temp = Top;
+
+                data.RemoveAt(Size - 1);
+                Console.WriteLine($"\n\tThe '{temp}' was removed from Stack. STACK size = {Size}.\n");
+       
+            }
+            catch
+            {
+                 Console.WriteLine("\n\tThe STACK IS NULL... \n");                
+            }
+        }
+    }
+}
